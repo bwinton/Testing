cbd21922767a7ea53218c33caf6fbdee820af3c2
Blake Winton <bwinton@latte.ca>
2012-05-10 14:30:09 -0400

    Add redis!

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+redis.set('foo', 'bar');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  redis.get('foo', function(err, value) {
+    response.send('Hello World!<br>foo is: ' + value);
+  });
 });
 
 var port = process.env.PORT || 3000;
